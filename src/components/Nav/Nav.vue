c0fa91977e186b9e083e293bcb63d03c0f98a535
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-09-05 01:12:48 +0800

    More animated navbar

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,9 +1,10 @@
 
 <template>
-    <div
-    v-motion :initial="{ opacity: 0 }" :enter="{  opacity: 1, transition: { duration: 1000, delay: 1400, type:'keyframes', ease: 'easeInOut' }}"
-    class="fixed right-0 flex flex-row gap-10 text-zinc-400 lg:pr-44 p-4">
-        <a v-for="link in navLinks" :href="link.link" class="hover:text-zinc-600 transition-all duration-200">
+    <div class="fixed right-0 flex flex-row gap-10 text-zinc-500 lg:pr-44 p-4">
+        <a v-for="(link, index) in navLinks" :key="index"
+        v-motion :initial="{ opacity: 0 }" :enter="{  opacity: 1, transition: { duration: 600, delay: 600 * (index + 1), type:'keyframes', ease: 'easeInOut' }}"
+        :href="link.link"
+        class="hover:text-zinc-600 transition-all duration-200">
             {{ link.label }}
         </a>
     </div>
