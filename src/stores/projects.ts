82bb56f03f0423aaf0819040826cf6422e421f16
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-10-23 17:45:27 +0800

    Build

--- src/stores/projects.ts
+++ src/stores/projects.ts
@@ -3,22 +3,31 @@ import { ref } from "vue";
 import { getDatabase, get, ref as fbref, onValue } from "firebase/database";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
+interface Project {
+    name : string,
+    github : string,
+    homepage : string,
+    description: string,
+    order: number,
+    preview: string
+} 
+
 export const useProjectStore = defineStore("projects", () => {
     const data = ref([] as any[]);
     const dataMap = ref({})
-    const selectedProject = ref("");
+    const selectedProject = ref({} as Project);
 
     const db = getDatabase();
     const analytics = getAnalytics();
 
     const projectsRef = fbref(db, 'projects/');
-
+    
     onValue(projectsRef, (snapshot) => {
         dataMap.value = snapshot.val()
-        data.value = Object.values(dataMap.value).sort((a, b) => a.order - b.order)
+        data.value = Object.values(dataMap.value).sort((a : any, b : any) => a.order - b.order)
     });
 
-    function setSelected(selected: string) {
+    function setSelected(selected: any) {
         selectedProject.value = selected
     }
 
