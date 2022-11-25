8ab78b7499ea9b7864c5b32b130afe1a8f0f68b5
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-11-25 16:11:40 +0800

    Reorganized files, added curved transitions between sections

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,44 +1,51 @@
-
 <template>
-    <div class="fixed w-full flex flex-row bg-zinc-50 z-50">
-        <div class="mx-auto lg:ml-auto flex flex-row gap-10 text-zinc-500 p-5">
-            <a v-for="(link, index) in navLinks" :key="index"
-            v-motion :initial="{ opacity: 0 }" :enter="{  opacity: 1, transition: { duration: 300, delay: 200 * (index), type:'keyframes', ease: 'easeInOut' }}"
-            @click="gotoLink(link.link)"
-            class="hover:text-zinc-600 cursor-pointer transition-all duration-200">
-                {{ link.label }}
-            </a>
-        </div>
+  <div class="w-full flex flex-row bg-zinc-50">
+    <div class="mx-auto lg:ml-auto flex flex-row gap-10 text-zinc-600 p-5">
+      <a
+        v-for="(link, index) in navLinks"
+        :key="index"
+        v-motion
+        :initial="{ opacity: 0 }"
+        :enter="{
+          opacity: 1,
+          transition: {
+            duration: 300,
+            delay: 200 * index,
+            type: 'keyframes',
+            ease: 'easeInOut',
+          },
+        }"
+        @click="gotoLink(link.link)"
+        class="hover:text-zinc-900 cursor-pointer transition-all duration-200"
+      >
+        {{ link.label }}
+      </a>
     </div>
+  </div>
 </template>
 <script setup lang="ts">
-import { useProjectStore } from '@/stores/projects';
+import { useProjectStore } from "@/stores/projects";
 
 const projectStore = useProjectStore();
 
 function gotoLink(data: string) {
-    projectStore.analyticsNavbarClicked(data)
-    window.location.href = data;
+  projectStore.analyticsNavbarClicked(data);
+  window.location.href = data;
 }
 
 const navLinks = [
-    {
-        label: "Home",
-        link: "#blurb"
-    },
-    {
-        label: "About",
-        link: "#aboutMe"
-    },
-    {
-        label: "My Projects",
-        link: "#projects"
-    },
-    
-]
-
-
+  {
+    label: "Home",
+    link: "#blurb",
+  },
+  {
+    label: "About",
+    link: "#aboutMe",
+  },
+  {
+    label: "My Projects",
+    link: "#projects",
+  },
+];
 </script>
-<style>
-
-</style>
+<style></style>
---
+++ src/components/SlideProfile/SlideProfile.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="w-screen h-screen">
+    <div
+      class="px-auto py-44"
+      v-motion
+      :initial="{ opacity: 0 }"
+      :enter="{
+        opacity: 1,
+        transition: {
+          duration: 1000,
+          delay: 400,
+          type: 'keyframes',
+          ease: 'easeInOut',
+        },
+      }"
+    >
+      <div class="w-full mx-auto mb-12">
+        <img
+          v-motion
+          :initial="{ y: -2400 }"
+          :enter="{
+            y: 0,
+            transition: {
+              duration: 3000,
+              delay: 1300,
+              type: 'spring',
+              mass: 0.2,
+            },
+          }"
+          src="https://assets.rewelcabiles.me/portfolio/me.jpeg"
+          class="rounded-full border-4 border-zinc-900 shadow-xl mx-auto h-40 h-40"
+        />
+        <iconListVue id="linkSmalls" class="mt-4 mx-auto w-fit"></iconListVue>
+      </div>
+      <div class="md:text-5xl text-2xl text-zinc-900 text-center">
+        Hi! I'm Rewel Cabiles
+      </div>
+      <div
+        class="md:ml-5 mt-6 text-lg text-zinc-900 lg:px-0 px-10 pb-10 text-center"
+      >
+        I’m a software & web developer based in Hong Kong / Philippines
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import iconListVue from "@/components/SlideProfile/iconList.vue";
+</script>
+
+<style></style>
